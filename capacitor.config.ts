9e2c9887a737ac4072a25962a8543d907d2350c1
AshutoshKumaar <sinhapintu545@gmail.com>
2025-08-30 01:29:08 +0530

    Fix Codemagig

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.digitalmenu.billmenu',
   appName: 'Bill Menu',
-  webDir: 'dist',  // dummy dist folder
+  webDir: '.next', 
   server: {
     url: "https://digital-menu-ivory-gamma.vercel.app/", // 👈 apna hosted URL
     cleartext: true
